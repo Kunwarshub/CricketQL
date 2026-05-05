4959b0e9e13d03a28cc38cbc79f55dc21bab7a62
unknown <kunwarabhey123@gmail.com>
2026-05-06 02:49:34 +0530

    Modified apps.py for render's fuckery

--- text2query/core/apps.py
+++ text2query/core/apps.py
@@ -1,9 +1,11 @@
 from django.apps import AppConfig
 
-
 class CoreConfig(AppConfig):
     name = 'core'
 
     def ready(self):
-        from .services.extract_names import load_player_names
-        load_player_names()
+        try:
+            from .services.extract_names import load_player_names
+            load_player_names()
+        except Exception:
+            pass
